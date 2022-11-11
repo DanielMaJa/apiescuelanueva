a434d9a29950cac6762d0446495ddf9965c0a603
DanielMaJa <85467543+DanielMaJa@users.noreply.github.com>
2022-11-11 13:09:59 -0500

    Update DistritoController.java
    Se eliminó el findAll del controlador

--- src/main/java/pe/com/apiescuelanueva/controller/DistritoController.java
+++ src/main/java/pe/com/apiescuelanueva/controller/DistritoController.java
@@ -28,10 +28,6 @@ public class DistritoController {
     private DistritoService distritoService;
     
     // @GetMapping sirve para obtener valores
-    @GetMapping
-    public List<DistritoEntity> findAll(){
-        return distritoService.findAll();
-    };
     
     @GetMapping("/custom")
     public List<DistritoEntity> findAllCustom(){
